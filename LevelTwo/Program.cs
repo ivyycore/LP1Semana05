34cb47f501c574af7d8fcce0d320e12715086159
ivyycore <mariasequeira.dev@gmail.com>
2025-03-21 13:56:03 +0000

    Made the else clause for if inputs are inserted

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -13,6 +13,13 @@ namespace LevelTwo
                 CanvasImage default_image = new CanvasImage("tux.jpg");
                 default_image.MaxWidth(24); //max size
                 AnsiConsole.Write(default_image); // renders in the console
+
+            }
+            else
+            {
+                CanvasImage input_image = new CanvasImage(args[0]);
+                input_image.MaxWidth(int.Parse(args[1]));
+                AnsiConsole.Write(input_image);
             }
         }
     }
