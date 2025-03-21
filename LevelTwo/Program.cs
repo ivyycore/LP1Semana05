68a31153397502008bbe4510d9cb7ae68fb0baa4
ivyycore <mariasequeira.dev@gmail.com>
2025-03-21 13:52:13 +0000

    Finished the if for no arguments input

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -8,6 +8,11 @@ namespace LevelTwo
         {
             Console.WriteLine(SoGoodClass.SoGoodMethod());
             if (args.Length == 00)
+            {
+                CanvasImage default_image = new CanvasImage("tux.jpg");
+                default_image.MaxWidth(24);
+                AnsiConsole.Write(default_image);
+            }
         }
     }
 }
