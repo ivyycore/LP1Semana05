7e136460e883dbefe7463f8d4cb46106fa7b511f
ivyycore <mariasequeira.dev@gmail.com>
2025-03-21 14:20:10 +0000

    Did the table

--- WorkerTable/Program.cs
+++ WorkerTable/Program.cs
@@ -10,6 +10,20 @@ namespace WorkerTable
         {
             Randomizer.Seed = new Random(int.Parse[0]); // Definir seed
             Faker faker = new Faker("pt_PT"); //Usar o objeto faker para gerar dados
+
+            // Create a table
+            var table = new Table();
+
+            // Add some columns
+            table.AddColumn("Column1");
+            table.AddColumn(new TableColumn("Column2").Centered());
+
+            // Add some rows
+            table.AddRow("Row1");
+            table.AddRow(new TableRow("Row2").Centered());
+
+            // Render the table to the console
+            AnsiConsole.Write(table);
         }
     }
 }
