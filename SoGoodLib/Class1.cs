1c989dbffcb5ae392f96a698d9339147ca8c9b7b
ivyycore <mariasequeira.dev@gmail.com>
2025-03-21 15:01:27 +0000

    finished sogoodclass and lib

--- SoGoodLib/Class1.cs
+++ SoGoodLib/Class1.cs
@@ -4,6 +4,10 @@ namespace SoGoodLib
 {
     public class SoGoodClass
     {
+         public static string SoGoodMethod()
+        {
+            return "I came from another project!";
+        }
 
     }
 }
